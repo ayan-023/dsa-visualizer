0cd468fb3a38ff6ac2de61d29a344bdd1e67111b
Ayan <ayankhanpcc@gmail.com>
2026-07-07 17:02:48 +0530

    Initial commit: working stack visualizer with index rail

---
+++ script.js
@@ -0,0 +1,34 @@
+let stack = []
+
+function render() {
+    const container = Document.getElementById('stackContainer');
+    container.innerHtml='';
+    
+    stack.forEach((value, index)=>{
+        const box= document.createElement('div');
+        box.className='stack-box';
+        box.textContent=value;
+
+        if (index === stack.length -1){
+            box.classList.add('top')
+        }
+        container.appendChiled(box);
+    });
+}
+
+function pushValue() {
+    const input = document.getElementsById('valueInput');
+    if(input.value === '') return;
+
+    stack.push(Number(input.value));
+    input.value='';
+    render();
+}
+
+function popValue() {
+    if(stack.length === 0) return;
+    stack.pop();
+    render();
+}
+
+render();
